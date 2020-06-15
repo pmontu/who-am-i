37ae2981c8363b5404de214c51ff0fafd5097c2b
pmontu <pmontu@gmail.com>
2020-06-15 17:22:44 +0530

    Updates

--- precache-manifest.067ac3f32d4f1426ac2613ac58cc4568.js
+++ precache-manifest.067ac3f32d4f1426ac2613ac58cc4568.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f472f0806014289d47e0a478fc917487",
+    "revision": "17466d878434113cf6d51a8c12f2cebb",
     "url": "/who-am-i/index.html"
   },
   {
-    "revision": "16d1b2b6f838796313d3",
+    "revision": "8ec8ced7d47c8f0d0f09",
     "url": "/who-am-i/static/css/2.34087573.chunk.css"
   },
   {
-    "revision": "55ec9c00d986b2942cb2",
+    "revision": "7db02f4c656cdca90051",
     "url": "/who-am-i/static/css/main.45c84b11.chunk.css"
   },
   {
-    "revision": "16d1b2b6f838796313d3",
-    "url": "/who-am-i/static/js/2.aaac95d2.chunk.js"
+    "revision": "8ec8ced7d47c8f0d0f09",
+    "url": "/who-am-i/static/js/2.4675b75c.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/who-am-i/static/js/2.aaac95d2.chunk.js.LICENSE.txt"
+    "url": "/who-am-i/static/js/2.4675b75c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55ec9c00d986b2942cb2",
-    "url": "/who-am-i/static/js/main.5a25e795.chunk.js"
+    "revision": "7db02f4c656cdca90051",
+    "url": "/who-am-i/static/js/main.72ecab22.chunk.js"
   },
   {
     "revision": "3986d5dd8752449acbd0",
